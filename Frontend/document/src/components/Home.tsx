80b59fb5e16028018bd4866c9e04dcfff6228a81
Hamza Butt <hamzambutt1@gmail.com>
2022-05-16 01:05:56 +0100

    CSS changes, added profile component

--- Frontend/document/src/components/Home.tsx
+++ Frontend/document/src/components/Home.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import Profile from './Profile'
 
 
 const Home = () => {
 
   return (
-  <>
-    <section className="home-page">
-      <div className="left-section"></div>
-      <div className="right-section"></div>
-    </section>
-</>
+    <>
+
+      <section className="main-page">
+        <div className="home-page">
+          <div className="left-section">
+            <Profile />
+          </div>
+          <div className="right-section">
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
