d24781c903b37ce3eab45838c439d2e0fb8b549f
Hamza Butt <hamzambutt1@gmail.com>
2023-03-11 12:59:04 +0100

    chat popup created

--- Frontend/document/src/components/ConvoBanner.tsx
+++ Frontend/document/src/components/ConvoBanner.tsx
@@ -4,33 +4,36 @@ import React, { useState } from 'react'
 const ConvoBanner = () => {
 
   let [popupTest, setPopupTest] = useState(true)
-  
+  let [stringName, setStringName] = useState('chat-popup-unread')
+  let [messages, setMessages] = useState({
+    name: "Hamza",
+    message: "Hello, how are you doing?"
+  })
 
   /*currently not used, will be used when useEffect comes in.*/
   // let [popUp, setpopUp] = useState(true)
 
-  
-  const handleOnClick = (event: any) => {
-    setPopupTest(!popupTest)
-    
-  }
 
+  const handleOnClick = (event: any) => {
+    // setPopupTest(!popupTest)
+    setStringName('chat-popup-read')
+  }
 
   return (
     <div className="convo-section">
       { popupTest &&
         <div
-          className="chat-popup"
+          className={stringName}
           onClick={handleOnClick}
         >
           <div className="chat-name">
-            Hamster
-      </div>
+            {messages.name}
+          </div>
           <div className="chat-message">
-            hi, hello, how are you
-      </div>
+            {messages.message}
+          </div>
         </div>
-  }
+      }
     </div>
 
   )
