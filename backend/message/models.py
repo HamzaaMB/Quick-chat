f00bdf0ae435f1809c39df02de6669041fb7635d
Hamza Butt <hamzambutt1@gmail.com>
2023-03-15 17:02:10 +0100

    simple get request with django endpoint created. Returns string of hello after making get request to api/message.Now requires passing of JSON

--- backend/message/models.py
+++ backend/message/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    name: models.CharField(max_length=100)
+    message: models.CharField(max_length=100)
--- backend/message/views.py
+++ backend/message/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Message
+from .serializers.common import MessageSerializer
+
+class MessageView(APIView):
+    def get(self, _request):
+        messages = Message.objects.all()
+        serialized_messages = MessageSerializer(messages, many=True)
+        return Response('hello', status=status.HTTP_200_OK)
+
+
